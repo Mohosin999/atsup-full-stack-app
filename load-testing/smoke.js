94b4ef8880b9e01b7cd68c0cf796426ccdb84d29
Mohosin999 <mohosin.hasan.akash@gmail.com>
2026-09-26 17:16:02 +0600

    Add non-functional load testing using k6 package

---
+++ load-testing/smoke.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  vus: 1,
+  iterations: 1,
+};
+
+export default function () {
+  const res = http.get('http://localhost:5000/health');
+
+  check(res, {
+    'status 200': (r) => r.status === 200,
+  });
+}
+
